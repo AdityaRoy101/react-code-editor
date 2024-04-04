fda9fd3cc6dae54f3a94bcce0e33487e79b75335
Aditya Roy <86155138+AdityaRoy101@users.noreply.github.com>
2024-04-04 20:44:09 +0530

    Updated name on Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="fixed h-8 bottom-0 px-2 py-1 flex items-center justify-center w-full text-xs text-gray-500 ">
-      <span className="  text-slate-300 text-xs font-semibold">Buit with❤️By Aditya Roy(KIIT-24)</span>
+      <span className="  text-slate-300 text-xs font-semibold">Built with❤️By Aditya Roy(KIIT-24)</span>
     </div>
   );
 };
